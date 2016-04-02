ed646995e356ca66d0585a8aaa7801a4fee199e6
Justin <me@justinlam.ca>
2016-04-02 18:58:42 -0400

    Created basic command option to create custom config file;

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-var cmdLine = require('command-line-args');
-var cfg = require('./lib/utils/configManager');
+#!/usr/bin/env node
 
+var cmd = require('commander');
+var cfgMan = require('./lib/utils/configManager');
 
+cmd
+  .version('0.0.1')
+  .description('An application for collating files into predefined folders')
+  .option('-i, --initconfig', 'Initialize custom config file')
+  .parse(process.argv);
+
+if(cmd.initconfig){
+  cfgMan.createConfig('.');
+  console.log('> Custom configuration file created');
+} else{
+
+}
